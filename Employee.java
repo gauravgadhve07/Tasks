e5838587713658311de63023571ba902ce29ddfe
gauravgadhve07 <gauravgadhve07@gmail.com>
2025-12-16 16:17:25 +0530

    "added new tasks completions"

---
+++ Employee.java
@@ -0,0 +1,33 @@
+
+public class Employee {
+	
+	public static void main(String[] args) {
+		
+		// here we have to create the local variable
+		String name01 = "Ram kumar";
+		String eMail = "ramkumar@gmail.com";
+		int mobNo = 345645566;
+		short empID = 4544;
+		double salary = 45000.50;
+		float experience = 5.5f;
+		String companyName = "Microsoft";
+		String position = "Junior Developer";
+		String dateofBirth = " 5 June";
+		String gender = "Male";
+		
+		System.out.println("Employee Information: ");
+		System.out.println(" ");
+		System.out.println(" 1) Name of employee is: " + name01);
+		System.out.println(" 2) Email of employee is: " + eMail);
+		System.out.println(" 3) Mobile Number of employee is: " + mobNo);
+		System.out.println(" 4) Id of employee is: " + empID);
+		System.out.println(" 5) Salary of employee is: " + salary);
+		System.out.println(" 6) Experience of employee is: " + experience);
+		System.out.println(" 7) Company of employee is: " + companyName);
+		System.out.println(" 8) Position of employee is: " + position);
+		System.out.println(" 9) Date of birth is: " + dateofBirth);
+		System.out.println("10) Gender of employee is: " + gender);
+		
+	}
+
+}
---
+++ Student.java
@@ -0,0 +1,24 @@
+
+public class Student {
+	public static void main(String[] args) {
+		String name01 = "Gaurav Gadhave";
+		String email = "gauravgadhave@gmail.com";
+		int mobNo = 345645566;
+		short studentID = 4544;
+		String collageName = "srttc";
+		String gender = "Male";
+		String dateofBirth = " 27 June";
+		
+		System.out.println("Student Information: ");
+		System.out.println(" ");
+		System.out.println(" 1) Name of student is: " + name01);
+		System.out.println(" 2) Email of student is: " + email);
+		System.out.println(" 3) Mobile Number of student is: " + mobNo);
+		System.out.println(" 4)  ID of student is: " + studentID);
+		System.out.println(" 5) gender of student is: " + gender);
+		System.out.println(" 6)  date of birth is : " + dateofBirth);
+		System.out.println(" 7) collageName of student is: " + collageName);
+		
+	}
+
+}
