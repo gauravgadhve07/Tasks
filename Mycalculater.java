7a0e63c35f5a48a90df506edc9b40bfa7eb1c991
gauravgadhve07 <gauravgadhve07@gmail.com>
2025-12-17 15:36:31 +0530

    Add files via upload

---
+++ Mycalculater.java
@@ -0,0 +1,23 @@
+
+public class Mycalculater {
+public static void main(String[] args) {
+	
+	// TOday we learn about Opretors
+	
+	int a = 5;
+	int b = 7;
+	int c = a + b ;
+	int f = a-b ;
+	int g = a * b ;
+	int h = a / b ;
+	int I = a % b ;
+	
+	System.out.println("Addition of " + a+ "and "+b+ "is: "+c);
+	System.out.println("Substration of " + a+ "and "+b+ "is: "+f);
+	System.out.println("multiplication of " + a+ "and "+b+ "is: "+g);
+	System.out.println("devision of " + a+ "and "+b+ "is: "+h);
+	System.out.println(" Modulsof " + a+ "and "+b+ "is: "+I);
+	
+	
+}
+}
